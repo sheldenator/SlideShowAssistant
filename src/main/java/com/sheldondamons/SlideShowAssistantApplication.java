0e099c0d42004d62c29525fe40872cb2faa6cb9d
Sheldon Damons <sdamons@hidglobal.com>
2019-02-05 11:42:55 +0800

    Spring Boot skeleton

---
+++ src/main/java/com/sheldondamons/SlideShowAssistantApplication.java
@@ -0,0 +1,14 @@
+package com.sheldondamons;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideShowAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlideShowAssistantApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/sheldondamons/SlideShowAssistantApplicationTests.java
@@ -0,0 +1,17 @@
+package com.sheldondamons;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SlideShowAssistantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
